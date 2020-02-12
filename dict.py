1bdeed6e3102894fd26794671973a193f8c45207
grtcoder <divyanshumathpal@gmail.com>
2020-02-12 22:54:34 +0530

    basic structure of code

---
+++ dict.py
@@ -0,0 +1,27 @@
+import sys
+filename=sys.argv[1]#commands like python <file_name> (<risc-v code path>) => sys.argv[1]
+if(not filename.endswith('.asm')):
+    print("This file format is invalid")
+    sys.exit()
+f=open(filename,'r+')
+lines=f.read().splitlines()
+terminate=False
+def firstoc(str,char):
+    for i in range(len(str)):
+        if str[i]==char:
+            return i
+    return -1
+pc=0
+# def isvalid(command):
+#     True if command is found in dict, false otherwise
+# def update_pc(command,inp1): 
+while not terminate:
+    v=firstoc(lines[pc],' ')
+    command,inp=lines[pc][:v],lines[pc][v+1:]
+    #isvalid()
+    input_arguments=inp.split(',')
+    if(len(input_arguments)>3):
+        print('Too many Arguments')
+        sys.exit()
+    
+f.close()
