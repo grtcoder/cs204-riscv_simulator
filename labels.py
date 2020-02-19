2dc1fb5649b15f0ab783cc37379f029837171537
Akshay Gahlot <54377715+akshaygahlot73@users.noreply.github.com>
2020-02-19 14:35:26 +0530

    labels.py
    get labels

---
+++ labels.py
@@ -0,0 +1,22 @@
+import sys
+filename=sys.argv[1]#commands like python <file_name> (<risc-v code path>) => sys.argv[1]
+if(not filename.endswith('.asm')):
+    print("This file format is invalid")
+    sys.exit()
+f=open(filename,'r+')
+lines=f.read().splitlines()
+ct = 0
+dict = {}
+for i in lines:
+    ln = len(i)
+    lbl = ""
+    flag = 0
+    for j in range(ln):
+        if i[j] == ':':
+            flag = 1
+            break
+        lbl += i[j]
+    if flag == 1:
+        dict[lbl] = ct 
+    ct += 4
+print(dict)
