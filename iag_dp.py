ba27563a326c1a9491a3f38120ae2de6f87d6648
PradyumnG <allcodingwebsites@gmail.com>
2020-03-23 16:49:30 +0530

    Decode file and some changes to iag

--- iag_dp.py
+++ iag_dp.py
@@ -1,34 +1,32 @@
-
 #control signals pc_select, pc_enable, inc_select are assumed to be given and ra and imm are in binary
 #if ra and imm are not in binary, we need to make a function to convert them into binary
 #all values are represented as binary strings
+# curr program counter is either passed or stored as a global variable(binary string)
 
-temp_pc = "0"
+def to_binary(a):
+    s = ""
+    while (a):
+        s += str(a%2)
+        a = a//2
+    return ("".join(reversed(s)))
 
-def iag(pc_select, pc_enable, inc_select, imm, ra, next_pc):
-    mux_inc = "0"
-    mux_pc = ""
+def to_decimal(a):
+    return int(a,base=2)
+
+def iag(pc_select, pc_enable, inc_select, imm, ra,curr_pc):#pass line difference in imm field
+    temp_pc = ""
+    mux_inc = 4
+    mux_pc = 0
     if(pc_select == 0):
         temp_pc = ra
     else:
-        temp_pc = next_pc
+        temp_pc = curr_pc
     if(pc_enable == 1):
         if(inc_select == 0):
-            mux_inc = "100"
+            mux_inc = 4
         else:
-            mux_inc = imm
-        while(mux_inc.size()<temp_pc.size()):
-            mux_inc = '0'+mux_inc
-        while(temp_pc.size()<mux_inc.size()):
-            temp_pc = '0'+temp_pc
-        i = temp_pc.size()-1
-        s = 0
-        while(i>=0):
-            a = temp_pc[i]-'0'
-            b = mux_inc[i]-'0'
-            mux_pc = '0' + (a^b^s)
-            s = (a&b)|(b&s)|(a&s)
-            i = i-1
-        # returns temp_pc for mux y and next_pc which can be used to call this function again
-        pc = [temp_pc, mux_pc]
-        return pc
+            mux_inc = to_decimal(imm)
+        mux_pc = to_decimal(temp_pc) + mux_inc
+        mux_pc = to_binary(mux_pc)
+        # returns mux_pc for mux y
+        return mux_pc
