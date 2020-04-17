7cc4046d840dda8e5df527ddc02a50e86593d24b
ankitbhadu <2018csb1073@iitrpr.ac.in>
2020-04-17 13:25:06 +0530

    made new decode for phase3 funct name is decode3

--- Buff_Regs.py
+++ Buff_Regs.py
@@ -17,7 +17,10 @@ def fetch(pc):
 	for i in range(len(machine_code[pc])):
 		MC.append(int(machine_code[pc][i]))
 	return MC
+@dataclass
 class PIP_REG:# buffer reg between deccode and execute 
+	instruction: int #mathpal dekhlena iska type and insert value here before doing IR.insert(0,temp)
+	type:String
 	pc:int=0
 	RA:int# these RA RB RZ are datapaths registers
 	RB:int
@@ -28,27 +31,28 @@ class PIP_REG:# buffer reg between deccode and execute
 	b_SELECT:int# used in alu, tells whether to take imm or register
 	pc_select:int 
 	inc_select:int 
-	Y_SELECT:int
+	Y_SELECT:int#not useful as of now
 	mem_read:int
+	memqty:int
 	mem_write:int
-	RF_WRITE:int
-	adress_a:int#rs1
-	adress_b:int#rs2
-	adress_c:int#rd
-	return_add:int
+	RF_WRITE:int#not useful as of now
+	address_a:int#rs1
+	address_b:int#rs2
+	address_c:int#rd
+	return_add:int#not used as of now
 	branchTaken:bool=False
 	isFlushed:bool=False
 	isBranchInstruction:bool=False
 	isLoad:bool=False
 	isStore:bool=False
-	isALU:bool=False
+	isALU:bool=False#lui and auipc true or false? right now i've taken it true!
 	isJump:bool=False #jal and jalr
 	isnull:False
     #above boolean will help us easily identify and take action for hazards
 	stall:0
 	state:1
-	enable:int=0
-	enable2:int=1
+	enable:int=0#not useful as of now
+	enable2:int=1#not useful as of now
 IR=[]
 data_hazard=0
 stalls_data_hazard=0
@@ -67,7 +71,7 @@ def run():
 			if IR[i].state==1:
 				IR[i].instruction=fetch(pc)
 			elif IR[i].state==2 and IR[i].isFlushed == False:
-				IR[i].type, IR[i].b_select, IR[i].ALU_op, IR[i].mem_read, IR[i].mem_write, IR[i].reg_write, IR[i].memqty, IR[i].pc_enable, IR[i].pc_select, IR[i].inc_select=decode(IR[i].instruction)
+				 IR[i]=decode(IR[i].instruction)
 			elif IR[i].state==3 and IR[i].isFlushed == False:
 				alu(IR[i].instruction,IR[i].alu_op,IR[i].b_select,IR[i].ins_type)
 			elif IR[i].state==4 and IR[i].isFlushed == False:
@@ -87,60 +91,60 @@ def run():
 			
 #data hazard handling
 
-#below threee are just for data fwding
+#below three are just for data fwding
 #below four are sufficient for data fwding logic need to write for stalling
 def ForwardDependency_EtoE(IR):
-		if (IR[2].addressC == 0):#EX-MEM's rd=0
+		if (IR[2].address_c == 0):#EX-MEM's rd=0
 			return
 		if (IR[2].isJump == False and IR[2].isALU == False ):#EX-MM isnt alu and jal jalr
 			return
 
-		if (IR[1].addressA == IR[2].addressC and IR[1].addressB == IR[2].addressC): #rd of exmem = rs1 and rs2 of id_ex
+		if (IR[1].address_a == IR[2].address_c and IR[1].address_b == IR[2].address_c): #rd of exmem = rs1 and rs2 of id_ex
 			print("inside EtoE-1")
 			data_hazard=data_hazard+1
 			IR[1].RA = IR[2].RZ
 			IR[1].RB = IR[2].RZ
-		if (IR[1].addressA == IR[2].addressC):#rd 0f exmem = rs1 of id_ex
+		if (IR[1].address_a == IR[2].address_c):#rd 0f exmem = rs1 of id_ex
 			print("inside EtoE-2")
 			data_hazard=data_hazard+1
 			IR[1].RA = IR[2].RZ
 			return
-		if (IR[1].addressB == IR[2].addressC):#rd of exmem = rs2 of id_ex
+		if (IR[1].address_b == IR[2].address_c):#rd of exmem = rs2 of id_ex
 			print("inside EtoE- 3") 
 			data_hazard=data_hazard+1
 			IR[1].RB = IR[2].RZ
 			return
 		return
 def ForwardDependency_MtoE():
-		if (IR[3].addressC == 0):
+		if (IR[3].address_c == 0):
 			return
 
-		if (IR[1].addressB == IR[3].addressC and IR[1].addressA == IR[3].addressC):
+		if (IR[1].address_b == IR[3].address_c and IR[1].address_a == IR[3].address_c):
 			print( "inside 1 MtoE" )
 			data_hazard=data_hazard+1
 			IR[1].RA = IR[3].RY
 			IR[1].RB = IR[3].RY
 			return
-		if (IR[1].addressA == IR[3].addressC):
+		if (IR[1].address_a == IR[3].address_c):
 			print( "inside 2 MtoE" )
 			data_hazard=data_hazard+1
 			IR[1].RA = IR[3].RY
 			return
-		if (IR[1].addressB == IR[3].addressC):
+		if (IR[1].address_b == IR[3].address_c):
 			print( "inside 3 MtoE" )
 			data_hazard=data_hazard+1
 			IR[1].RB = IR[3].RY
 			return
 		return
 def ForwardDependencyMtoM():
-		if (IR[3].addressC == 0):
+		if (IR[3].address_c == 0):
 			return
 		if (IR[3].isLoad == False):
 			return
 
 		if (IR[2].isStore == True):
 		#Load-Store wali Dependency
-			if (IR[2].addressB == IR[3].addressC):
+			if (IR[2].address_b == IR[3].address_c):
 				print ("MtoM") 
 				data_hazard=data_hazard+1
 				IR[2].RB = IR[3].RY
@@ -151,13 +155,13 @@ def ForwardDependencyMtoM():
 #this if for stalling it itself will increase cycle count
 def forward_dependency_MtoEStall():
 	
-		if (   IR[3].addressC == 0):
+		if (   IR[3].address_c == 0):
 			return  
 		if (   IR[3].isLoad == False):
 			return  
 		if (   IR[2].isStore == True):
 			return  
-		if (   IR[1].addressA ==    IR[3].addressC and    IR[1].addressB ==    IR[3].addressC):
+		if (   IR[1].address_a ==    IR[3].address_c and    IR[1].address_b ==    IR[3].address_c):
 			print("forward_dependecy_MtoEStall 1")  
 			data_hazard=data_hazard+1  
 			stalls_data_hazard=stalls_data_hazard+1
@@ -165,7 +169,7 @@ def forward_dependency_MtoEStall():
 			IR[1].RA =    IR[3].RY  
 			IR[1].RB =    IR[3].RY  
 			return
-		if (   IR[1].addressA ==    IR[3].addressC):
+		if (   IR[1].address_a ==    IR[3].address_c):
 		   
 			print("forward_dependecy_MtoEStall 2")  
 			data_hazard+=1 
@@ -174,7 +178,7 @@ def forward_dependency_MtoEStall():
 			IR[1].RA =    IR[3].RY  
 			return  
 		   
-		if (   IR[1].addressB ==    IR[3].addressC):
+		if (   IR[1].address_b ==    IR[3].address_c):
 		   
 			print("forward_dependecy_MtoEStall 3")  
 			data_hazard+=1 
---
+++ decode_phase3.py
@@ -0,0 +1,218 @@
+from Buff_regs import *
+def decode3(instruction):  # instruction comes as array of bits
+    #control signals
+    pipreg=PIP_REG()
+    pipreg.address_a=binary(reg[binary(instruction[12:17])])#rs1
+    pipreg.address_b=binary(reg[binary(instruction[7:12])])#rs2
+    pipreg.address_c=binary(reg[binary(instruction[20:25])])#rd
+    b_select = -1
+    ALU_op = -1
+    mem_read = -1
+    mem_write = -1
+    reg_write = -1
+    memqty = -1
+    pc_enable = 1
+    pc_select = 1
+    inc_select = 0
+    type = ""
+    # R-format ###### to add more
+    R_code = [0, 1, 1, 0, 0, 1, 1]
+    add_funct7 = [0, 0, 0, 0, 0, 0, 0]
+    sub_funct7 = [0, 1, 0, 0, 0, 0, 0]
+    and_funct7 = [0, 0, 0, 0, 0, 0, 0]
+    or_funct7  = [0, 0, 0, 0, 0, 0, 0]
+    sll_funct7 = [0, 0, 0, 0, 0, 0, 0]
+    slt_funct7 = [0, 0, 0, 0, 0, 0, 0]
+    sra_funct7 = [0, 1, 0, 0, 0, 0, 0]
+    srl_funct7 = [0, 0, 0, 0, 0, 0, 0]
+    xor_funct7 = [0, 0, 0, 0, 0, 0, 0]
+    mul_funct7 = [0, 0, 0, 0, 0, 0, 1]
+    div_funct7 = [0, 0, 0, 0, 0, 0, 1]
+    rem_funct7 = [0, 0, 0, 0, 0, 0, 1]
+    add_funct3 = [0, 0, 0]
+    sub_funct3 = [0, 0, 0]
+    and_funct3 = [1, 1, 1]
+    or_funct3  = [1, 1, 0]
+    sll_funct3 = [0, 0, 1]
+    slt_funct3 = [0, 1, 0]
+    sra_funct3 = [1, 0, 1]
+    srl_funct3 = [1, 0, 1]
+    xor_funct3 = [1, 0, 0]
+    mul_funct3 = [0, 0, 0]
+    div_funct3 = [1, 0, 0]
+    rem_funct3 = [1, 1, 0]
+    # I-format
+    I_code_load = [0, 0, 0, 0, 0, 1, 1]
+    I_code_arith = [0, 0, 1, 0, 0, 1, 1]
+    I_code_jalr = [1, 1, 0, 0, 1, 1, 1]
+    lb_funct3 = [0, 0, 0]
+    ld_funct3 = [0, 1, 1]
+    lh_funct3 = [0, 0, 1]
+    lw_funct3 = [0, 1, 0]
+    addi_funct3 = [0, 0, 0]
+    andi_funct3 = [1, 1, 1]
+    ori_funct3 = [1, 1, 0]
+
+    # S-format
+    S_code    = [0, 1, 0, 0, 0, 1, 1]
+    sb_funct3 = [0, 0, 0]
+    sw_funct3 = [0, 1, 0]
+    sh_funct3 = [0, 0, 1]
+    sd_funct3 = [0, 1, 1]
+
+    # SB-format
+    SB_code    = [1, 1, 0, 0, 0, 1, 1]
+    beq_funct3 = [0, 0, 0]
+    bne_funct3 = [0, 0, 1]
+    bge_funct3 = [1, 0, 1]
+    blt_funct3 = [1, 0, 0]
+    # U-format
+    auipc_op = [0, 0, 1, 0, 1, 1, 1]
+    lui_op = [0, 1, 1, 0, 1, 1, 1]
+    # UJ-format
+    UJ_code = [1, 1, 0, 1, 1, 1, 1]
+    opcode = instruction[25:32]
+    if opcode == R_code:
+        pipreg.isALU=True
+        b_select = 0
+        type = "R"
+        mem_read = 0
+        mem_write = 0
+        reg_write = 1
+        funct7 = instruction[0:7]
+        funct3 = instruction[17:20]
+        if funct7 == add_funct7 and funct3 == add_funct3:
+            ALU_op = 0
+        elif funct7 == sub_funct7 and funct3 == sub_funct3:
+            ALU_op = 1
+        elif funct7 == and_funct7 and funct3 == and_funct3:
+            ALU_op = 2
+        elif funct7 == or_funct7 and funct3 == or_funct3:
+            ALU_op = 3
+        elif funct7 == sll_funct7 and funct3 == sll_funct3:
+            ALU_op = 4
+        elif funct7 == slt_funct7 and funct3 == slt_funct3:
+            ALU_op = 5
+        elif funct7 == sra_funct7 and funct3 == sra_funct3:
+            ALU_op = 6
+        elif funct7 == srl_funct7 and funct3 == srl_funct3:
+            ALU_op = 7
+        elif funct7 == xor_funct7 and funct3 == xor_funct3:
+            ALU_op = 8
+        elif funct7 == mul_funct7 and funct3 == mul_funct3:
+            ALU_op = 9
+        elif funct7 == div_funct7 and funct3 == div_funct3:
+            ALU_op = 10
+        elif funct7 == rem_funct7 and funct3 == rem_funct3:
+            ALU_op = 11
+    elif opcode == I_code_arith:
+        type = "I"
+        pipreg.isALU=True
+        mem_read = 0
+        mem_write = 0
+        reg_write = 1
+        b_select = 1
+        funct3 = instruction[17:20]
+        if funct3 == addi_funct3:
+            ALU_op = 0
+        elif funct3 == andi_funct3:
+            ALU_op = 2
+        elif funct3 == ori_funct3:
+            ALU_op = 3
+    elif opcode == I_code_load:  # review...
+        type = "I"
+        pipreg.isLoad=True
+        mem_read = 1
+        mem_write = 0
+        reg_write = 1
+        b_select = 1
+        funct3 = instruction[17:20]
+        if funct3 == lb_funct3:
+            memqty = 1
+        elif funct3 == lh_funct3:
+            memqty = 2
+        elif funct3 == lw_funct3:
+            memqty = 4
+        elif funct3 == ld_funct3:
+            memqty = 8
+        ALU_op = 0
+    elif opcode == I_code_jalr:
+        type = "jalr"
+        pipreg.isJump=True
+        pc_select = 0
+        inc_select = 1
+        mem_read = 0
+        mem_write = 0
+        reg_write = 1
+        b_select = 1
+        ALU_op = 0
+    elif opcode == S_code:
+        type = "S"
+        pipreg.isStore=True
+        mem_read = 0
+        mem_write = 1
+        reg_write = 0
+        b_select = 1
+        ALU_op = 0
+        funct3 = instruction[17:20]
+        if funct3 == sb_funct3:
+            memqty = 1
+        elif funct3 == sh_funct3:
+            memqty = 2
+        elif funct3 == sw_funct3:
+            memqty = 4
+        elif funct3 == sd_funct3:
+            memqty = 8
+    elif opcode == SB_code:
+        b_select = 0
+        pipreg.isBranchInstruction=True
+        type = "SB"
+        mem_read = 0
+        mem_write = 0
+        inc_select = 1
+        reg_write = 0
+        funct3 = instruction[17:20]
+        if funct3 == beq_funct3:
+            ALU_op = 12
+        elif funct3 == bge_funct3:
+            ALU_op = 13
+        elif funct3 == blt_funct3:
+            ALU_op = 14
+        elif funct3 == bne_funct3:
+            ALU_op = 15
+    elif opcode == auipc_op:
+        type = 'auipc'
+        #todo
+        pipreg.isALU=True
+        mem_read = 0
+        mem_write = 0
+        reg_write = 1
+        b_select = 1
+    elif opcode == lui_op:
+        type = 'lui'
+        #todo
+        pipreg.isALU=True
+        mem_read = 0
+        mem_write = 0
+        reg_write = 1
+        b_select = 1
+    elif opcode == UJ_code:
+        type = "jal"
+        pipreg.isJump=True
+        b_select = 1
+        inc_select = 1
+        mem_read = 0
+        mem_write = 0
+        reg_write = 1
+    pipreg.b_select=b_select
+    pipreg.ALU_op=ALU_op
+    pipreg.mem_read=mem_read
+    pipreg.mem_write=mem_write
+    pipreg.reg_write=reg_write
+    pipreg.memqty=memqty
+    pipreg.pc_enable=pc_enable
+    pipreg.pc_select=pc_select
+    pipreg.inc_select=inc_select
+    pipreg.type=type
+    pipreg.immediate=get_immediate(instruction, type)
+    return pipreg
